93bd1f2e2edd66f41ac3f147c60164850ca7f73d
Oksana Bondareva <oksanchik252@gmail.com>
2025-01-23 15:47:24 +0300

    feat: add header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,15 @@
+import { AppBar, Typography } from "@mui/material";
+
+const Header = () => {
+    return (
+        <>
+            <AppBar>
+                <Typography variant="h3">
+                    ToDo List
+                </Typography>
+            </AppBar>
+        </>
+    )
+}
+
+export default Header;
